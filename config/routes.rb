0a77e628faf7ac6669433bc60d1c21d2b4d7e363
Wilmer Pellicier <wilmer.pellicier@gmail.com>
2020-11-26 17:03:47 -0500

    Created pages, layouts, and partials. Front page styling is mostly done. Used Bootsrap 3 for styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#front'
+  get '/about',               to: 'pages#about'
+  get '/student_login',       to: 'pages#student_login'
+  get '/student_home',        to: 'pages#student_home'
+  get '/student_projects',    to: 'pages#student_projects'
+  get '/student_evaluations', to: 'pages#student_evaluations'
+  get '/admin_login',         to: 'pages#admin_login'
+  get '/admin_home',          to: 'pages#admin_home'
+  get '/admin_classes',       to: 'pages#admin_classes'
+  get '/admin_class_home',    to: 'pages#admin_class_home'
+  get '/admin_team_overview', to: 'pages#admin_team_overview'
 end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,70 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get front" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Eval: Peer Evaluation"
+  end
+
+  test "should get about" do
+    get about_url_path
+    assert_response :success
+    assert_select "title", "About | Eval: Peer Evaluation"
+  end
+
+  test "should get student_login" do
+    get student_login_url_path
+    assert_response :success
+    assert_select "title", "Student Login | Eval: Peer Evaluation"
+  end
+
+  test "should get student_home" do
+    get student_home_url_path
+    assert_response :success
+    assert_select "title", "Student Home | Eval: Peer Evaluation"
+  end
+
+  test "should get student_projects" do
+    get student_projects_url_path
+    assert_response :success
+    assert_select "title", "Projects | Eval: Peer Evaluation"
+  end
+
+  test "should get student_evaluations" do
+    get tudent_evaluations_url_path
+    assert_response :success
+    assert_select "title", "Evaluations | Eval: Peer Evaluation"
+  end
+
+  test "should get admin_login" do
+    get admin_login_url_path
+    assert_response :success
+    assert_select "title", "Admin Login | Eval: Peer Evaluation"
+  end
+
+  test "should get admin_home" do
+    get admin_home_url_path
+    assert_response :success
+    assert_select "title", "Admin Home | Eval: Peer Evaluation"
+  end
+
+  test "should get admin_classes" do
+    get admin_classes_url_path
+    assert_response :success
+    assert_select "title", "Classes | Eval: Peer Evaluation"
+  end
+
+  test "should get admin_class_home" do
+    get admin_class_home_url_path
+    assert_response :success
+    assert_select "title", "Class Home | Eval: Peer Evaluation"
+  end
+
+  test "should get admin_team_overview" do
+    get admin_team_overview_url_path
+    assert_response :success
+    assert_select "title", "Team Overview | Eval: Peer Evaluation"
+  end
+
+end
