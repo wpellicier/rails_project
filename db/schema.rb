d79c27ca7ab7e7d767d53253effd8491f57a8abd
GavinHearns <ghearns101@gmail.com>
2020-11-28 21:33:15 -0500

    Changed ratings database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_074235) do
+ActiveRecord::Schema.define(version: 2020_11_29_022936) do
 
   create_table "admins", force: :cascade do |t|
     t.string "fname"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_074235) do
     t.integer "project_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "comment"
   end
 
   create_table "students", force: :cascade do |t|
