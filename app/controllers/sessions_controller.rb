34458cba0a65b5ed407b2451e536029931f06228
GavinHearns <ghearns101@gmail.com>
2020-11-30 01:34:33 -0500

    Linking login to student show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     student = Student.find_by(email: params[:session][:email].downcase)
     if student && student.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in student
+      redirect_to student
     else
       flash.now[:danger] = 'Invalid email/password combination' 
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,10 @@
 module SessionsHelper
+  def log_in(student)
+    session[:student_id] = student.id
+  end
+  def current_student
+    if session[:student_id]
+      @current_student ||= Student.find_by(id: session[:student_id])
+    end
+  end
 end
